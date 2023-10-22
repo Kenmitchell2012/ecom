0cc1166c9454522892242d28745ab57524dec89f
Kendrick Mitchell <kenmitchell2012@icloud.com>
2023-10-21 22:43:18 -0500

    added product page

--- store/views.py
+++ store/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
 
 # Create your views here.
 
@@ -9,3 +15,48 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+
+def login_user(request):
+    if request.method == "POST":
+        usernames = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=usernames, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("Welcome, " + user.username))
+            return redirect('home')
+
+        else:
+            messages.success(request, ("There was a problem logging in"))
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('You have been logged out'))
+    return redirect('home')
+ 
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            #login user
+            user = authenticate(username= username, password=password)
+            login(request, user)
+            messages.success(request, 'You have successfully created a account!!')
+            return redirect('home')
+        else:
+            messages.error(request, 'There was an error registering, please try again')
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form': form})
+    
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product': product})
