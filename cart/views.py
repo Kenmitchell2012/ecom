5aacce3db878373a853a8420adca7119acf0fff8
Kendrick Mitchell <39783561+Kenmitchell2012@users.noreply.github.com>
2023-11-05 00:31:09 -0500

    cart page update
    updated cart page

--- cart/views.py
+++ cart/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def cart_summary(request):
-    return render(request, 'cart_summary.html', {})
+    username = request.user.username
+    return render(request, 'cart_summary.html', {'username': username})
 
 def cart_add(request):
     pass
