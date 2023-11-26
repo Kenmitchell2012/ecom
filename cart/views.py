4517841396653190cadb1c5102a6b26e1df32205
Kendrick Mitchell <39783561+Kenmitchell2012@users.noreply.github.com>
2023-11-26 00:18:24 -0600

    add to cart

--- cart/views.py
+++ cart/views.py
@@ -20,9 +20,14 @@ def cart_add(request):
 
         # save to session
         cart.add(product=product)
+        
+        # get cart quantity
+        cart_quantity = cart.__len__()
 
         #return response
-        response = JsonResponse({'Product Name': product.name})
+        # response = JsonResponse({'Product Name': product.name})
+        response = JsonResponse({'qty': cart_quantity})
+
         return response
 
 def cart_delete(request):
